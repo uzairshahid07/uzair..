839548ee4481b209982bd17dac1ba18015bc7ce8
uzairshahid07 <uzairshahid510@gmail.com>
2024-09-22 16:59:01 +0500

    do not read

---
+++ main.dart
@@ -0,0 +1,72 @@
+void main() {
+
+// printTable(2);
+// printTable(3);
+// printTable(5);
+// print(printUzairShahid());
+// print(calculateMarks());
+// List students = [
+//   {
+//    "name": "uzair",
+//    "rollNo": "07",
+//    "sudject": {
+//     "English": "80",
+//     "Urdu": "85",
+//     "Math": "90",
+//    }
+//   },
+//   {
+//    "name": "umair",
+//    "rollNo": "08",
+//    "sudject": {
+//     "English": "70",
+//     "Urdu": "87",
+//     "Math": "89",
+//    }
+//   },
+//   {
+//    "name": "mubashir",
+//    "rollNo": "09",
+//    "sudject": {
+//     "English": "86",
+//     "Urdu": "80",
+//     "Math": "88",
+//    }
+//   }
+// ];
+// for (var student in students) {
+//   var result = calculateStudentMarks(student);
+//   student['percentage'] = result;
+// }
+
+// print(students);
+abc(0,0,20);
+abcV2(maths: 30);
+
+}
+abc([eng, urdu, maths]) {}
+abcV2({eng, urdu, maths}) {}
+// printTable(table) {
+// for (var i = 1; i < 10; i++) {
+//   print('$table * $i = ${table * i}');
+// }
+
+
+// }
+
+//  printUzairShahid() {
+//   print('Uzair Shahid');
+// }
+// dynamic calculateMarks() {
+//   var percentage = '100%';
+//   return percentage;
+// }
+// calculateStudentMarks(student) { 
+//   int totalMarks = 900;
+//   int obtainMarks = 
+//       student['subject']['English'] +  student['subject']['Urdu'] +  student['subject']['Math'];
+//   num percentage = obtainMarks / totalMarks * 100;
+//   print(percentage);
+//   return percentage;
+
+// }
